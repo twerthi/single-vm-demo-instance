80a80fe711ea1eca78066eb21bcfa1310bbd7dcc
Twerthi <twerthi@hotmail.com>
2026-05-29 15:30:36 -0700

    Fixing kind config file location

--- kind-cluster/install-kind.sh
+++ kind-cluster/install-kind.sh
@@ -1,2 +1,2 @@
 export KIND_EXPERIMENTAL_DOCKER_NETWORK="octopusdeploy_default"
-kind create cluster --config ~/kind-cluster/kind-config.yaml
+kind create cluster --config $PWD/kind-cluster/kind-config.yaml
