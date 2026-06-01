0c597abfd896fae3414959c9a1915c7655c64a7d
Twerthi <twerthi@hotmail.com>
2026-06-01 13:54:46 -0700

    Trying an even different approach

--- configure-offline.sh
+++ configure-offline.sh
@@ -1,5 +1,7 @@
+echo "Configuring local registry for offline installation..."
+docker run -d -p 5000:5000 --restart=always --name registry --network octopusdeploy_default registry:3
+
 echo "Pulling Argo CD images for offline installation..."
-mkdir $PWD/argocd/localimages
 
 curl -s https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml \
   | grep "image:" | sort -u | awk '{print $2}' \
@@ -9,11 +11,11 @@ curl -s https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/inst
       FILENAME="${PWD}/argocd/localimages/${NAME}_${TAG}.tar"
       echo "==> Pulling: $image"
       docker pull "$image"
-      docker save -o "$FILENAME" "$image"
+#      docker save -o "$FILENAME" "$image"
 
       echo "==> Pushing: $image to local registry"
-      #docker tag "$image" "localhost:5000/$image"
-      #docker push "localhost:5000/$image"
+      docker tag "$image" "localhost:5000/$image"
+      docker push "localhost:5000/$image"
       #kind load docker-image "$image" --name kind
-      kind load image-archive "$FILENAME" --name kind
+#      kind load image-archive "$FILENAME" --name kind
     done
