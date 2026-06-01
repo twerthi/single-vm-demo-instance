a568d1974f95adeb8e4f8cfc3fec0e8e412f9c2b
Twerthi <twerthi@hotmail.com>
2026-06-01 14:24:08 -0700

    Configuring offline mode for argocd

--- configure-offline.sh
+++ configure-offline.sh
@@ -6,12 +6,9 @@ echo "Pulling Argo CD images for offline installation..."
 curl -s https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml \
   | grep "image:" | sort -u | awk '{print $2}' \
   | while read image; do
-      NAME=$(echo "$image" | awk -F'/' '{print $NF}' | cut -d':' -f1)
-      TAG=$(echo "$image"  | awk -F':' '{print $NF}')
-      FILENAME="${PWD}/argocd/localimages/${NAME}_${TAG}.tar"
       echo "==> Pulling: $image"
       docker pull "$image"
-#      docker save -o "$FILENAME" "$image"
+
 
       echo "==> Pushing: $image to local registry"
       docker tag "$image" "localhost:5000/$image"
@@ -19,3 +16,15 @@ curl -s https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/inst
       #kind load docker-image "$image" --name kind
 #      kind load image-archive "$FILENAME" --name kind
     done
+
+echo "Patching Argo CD manifests to use local registry..."
+
+# Get all deployments in argocd namespace
+kubectl get deployments -n argocd -o name | while read deploy; do
+  echo "Patching $deploy..."
+  kubectl get "$deploy" -n argocd -o json \
+    | jq '(.spec.template.spec.containers[].image) |= 
+        gsub("quay.io"; "registry:5000/quay.io") |
+        gsub("ghcr.io"; "registry:5000/ghcr.io")' \
+    | kubectl apply -f -
+done
