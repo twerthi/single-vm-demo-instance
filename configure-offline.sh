00c4d6b9faf5e6be16a4fc09f98f69b9e42ae701
Twerthi <twerthi@hotmail.com>
2026-06-01 12:58:01 -0700

    Trying different method for argo in kind in offline mode

--- configure-offline.sh
+++ configure-offline.sh
@@ -1,13 +1,19 @@
 echo "Pulling Argo CD images for offline installation..."
+mkdir $PWD/argocd/localimages
 
 curl -s https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml \
   | grep "image:" | sort -u | awk '{print $2}' \
   | while read image; do
+      NAME=$(echo "$image" | awk -F'/' '{print $NF}' | cut -d':' -f1)
+      TAG=$(echo "$image"  | awk -F':' '{print $NF}')
+      FILENAME="${PWD}/argocd/localimages/${NAME}_${TAG}.tar"
       echo "==> Pulling: $image"
       docker pull "$image"
+      docker save -o "$FILENAME" "$image"
 
       echo "==> Pushing: $image to local registry"
       #docker tag "$image" "localhost:5000/$image"
       #docker push "localhost:5000/$image"
-      kind load docker-image "$image" --name kind
+      #kind load docker-image "$image" --name kind
+      kind load image-archive "$FILENAME" --name kind
     done
