e8cda18952b6e2df0468add7822e4efb8ddea4c8
Twerthi <twerthi@hotmail.com>
2026-06-01 15:39:41 -0700

    Updating patch script to contain statefulsets

--- configure-offline.sh
+++ configure-offline.sh
@@ -38,3 +38,23 @@ kubectl get deployments -n argocd -o name | while read deploy; do
       -p "[{\"op\":\"replace\",\"path\":\"/spec/template/spec/containers/0/image\",\"value\":\"$newimage\"}]"
   done
 done
+
+kubectl get statefulsets -n argocd -o name | while read deploy; do
+  echo "Patching $deploy..."
+  
+  # Get current images and build a patch
+  kubectl get "$deploy" -n argocd -o jsonpath='{.spec.template.spec.containers[*].name} {.spec.template.spec.containers[*].image}' | \
+  awk '{
+    n = NF/2
+    for (i=1; i<=n; i++) {
+      name = $i
+      image = $(i+n)
+      print name, image
+    }
+  }' | while read cname cimage; do
+    newimage="registry:5000/$cimage"
+    echo "  $cname: $cimage -> $newimage"
+    kubectl patch "$deploy" -n argocd --type=json \
+      -p "[{\"op\":\"replace\",\"path\":\"/spec/template/spec/containers/0/image\",\"value\":\"$newimage\"}]"
+  done
+done
